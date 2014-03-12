c3977e27a48b7d9e7a76d1a7e22ec36cd8569556
Jérémy Gignon <j.gignon@hexanet.fr>
2014-03-12 14:33:51 +0100

    Added ability to declare custom config parameters (route, file, scalar) in config.yml, as well as a simple config.js file to load

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -4,6 +4,7 @@ namespace Trsteel\CkeditorBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Exception\InvalidTypeException;
 
 /**
  * This is the class that validates and merges configuration from your app/config files
@@ -250,9 +251,29 @@ class Configuration implements ConfigurationInterface
                 ->end()
             ->end()
             ->children()
-                ->scalarNode('custom_config')
-                    ->defaultNull()
-                    ->info("The path of the custom config.js to use for the editor setup.")
+                ->arrayNode('custom_config')
+                    ->beforeNormalization()
+                    ->ifString()
+                        ->then(function($v) { return array("customConfig" => array('type' => 'file', 'value' => $v)); })
+                    ->end()
+                    ->prototype('array')
+                        ->children()
+                            ->scalarNode('type')
+                                ->isRequired()
+                                ->validate()
+                                ->ifNotInArray(array('file', 'route', 'scalar'))
+                                    ->thenInvalid('Invalid custom config type "%s"')
+                                ->end()
+                            ->end()
+                            ->scalarNode('value')
+                                ->isRequired()
+                            ->end()
+                            ->variableNode('route_parameters')
+                                ->defaultValue(array())
+                            ->end()
+                        ->end()
+                    ->end()
+                    ->info("The path of the custom config.js, or the list of custom config definitions to use for the editor setup.")
                 ->end()
             ->end()
             ->children()
