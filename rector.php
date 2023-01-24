b9503e83f9070de018c8d2205982a43c8ca90646
Trent Steel <tsteel@vivogroup.com.au>
2023-01-24 16:38:07 +1000

    Create rector.php

---
+++ rector.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Symfony\Set\SymfonyLevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__,
+    ]);
+
+    $rectorConfig->skip([
+        __DIR__.'/vendor',
+        __DIR__.'/Tests',
+    ]);
+
+    $rectorConfig->importNames();
+    $rectorConfig->importShortClasses(false);
+    $rectorConfig->parallel();
+
+    $rectorConfig->import(LevelSetList::UP_TO_PHP_81);
+    $rectorConfig->import(SymfonyLevelSetList::UP_TO_SYMFONY_60);
+    $rectorConfig->import(SymfonySetList::SYMFONY_CODE_QUALITY);
+
+    $services = $rectorConfig->services();
+
+    $services->set(AddVoidReturnTypeWhereNoReturnRector::class);
+};
